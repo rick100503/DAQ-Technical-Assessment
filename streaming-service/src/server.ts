c85525e324581e2b2806fcb5f0a7fdae7d89ebff
Eric Lin <elstar724@gmail.com>
2023-02-22 21:19:35 +1100

    aaa

--- streaming-service/src/server.ts
+++ streaming-service/src/server.ts
@@ -1,3 +1,4 @@
+import { error } from 'console';
 import net from 'net';
 import { WebSocket, WebSocketServer } from 'ws';
 
@@ -6,21 +7,38 @@ const TCP_PORT = parseInt(process.env.TCP_PORT || '12000', 10);
 const tcpServer = net.createServer();
 const websocketServer = new WebSocketServer({ port: 8080 });
 
+
+
 tcpServer.on('connection', (socket) => {
     console.log('TCP client connected');
     
     socket.on('data', (msg) => {
-        console.log(msg.toString());
+
+
+
+        let msg_string = msg.toString();
 
         // HINT: what happens if the JSON in the received message is formatted incorrectly?
         // HINT: see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/try...catch
-        let currJSON = JSON.parse(msg.toString());
 
+
+
+        try{
+            JSON.parse(msg_string)
+        }catch{
+            console.error(error);
+            msg_string = msg_string.slice(0,-1)
+        }
+        console.log("\n");
+        console.log(msg.toString());
+        console.log(msg_string);
+        let currJSON = JSON.parse(msg_string);
         websocketServer.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-              client.send(msg.toString());
-            }
-          });
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(msg_string);
+        }
+      });
+
     });
 
     socket.on('end', () => {
@@ -30,6 +48,8 @@ tcpServer.on('connection', (socket) => {
     socket.on('error', (err) => {
         console.log('TCP client error: ', err);
     });
+
+
 });
 
 websocketServer.on('listening', () => console.log('Websocket server started'));
