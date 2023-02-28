31ba19629b512ab4219fa56e4252cf3bec9b18e0
rick100503 <72172583+rick100503@users.noreply.github.com>
2023-02-28 14:05:37 +1100

    yesssssssss

--- battery-ui/src/recent_timestamps.tsx
+++ battery-ui/src/recent_timestamps.tsx
@@ -10,15 +10,15 @@ interface Outlierprops {
 function Recent_outliers({ timestamps } : Outlierprops){
     let valueColour = 'white';
     let html_string = "";
-    console.log(timestamps.length);
+    //console.log(timestamps.length);
     if (timestamps=== undefined || timestamps.length == 0){
+        return <li> </li>
+    }
+    else{
         for (let i = 0; i < timestamps.length;i++ ){
             html_string += "<li>" + timestamps[i] + "</li>"
         }
         return <ul dangerouslySetInnerHTML={{ __html: html_string }} />
-    }
-    else{
-        return <li> </li>
     }
 }
 
